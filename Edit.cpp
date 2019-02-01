bb7429ae2ad00094adc305da931a60b7c0f8490b
lyheang <lyheangung77@gmail.com>
2019-02-01 18:44:52 +0700

    add substraction function

--- Edit.cpp
+++ Edit.cpp
@@ -12,7 +12,15 @@ public:
     Fraction sub(Fraction);
     Fraction mul(Fraction);
     Fraction div(Fraction);
+
 };
 
+Fraction Fraction::sub(Fraction b){
+    denominator = denominator * b.denominator;
+    numerator = (numerator*b.denominator) -  (b.numerator*denominator);
+    return this;
+}
+
 main(){
+
 }
